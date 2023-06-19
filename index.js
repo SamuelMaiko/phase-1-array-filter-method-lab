c357f6dd697a78d33fc201fd2a4a81680a8ceee7
Samuel Maiko <sammymaiko721@gmail.com>
2023-06-19 22:51:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const findMatching = (drivers, name) => {
+    return drivers.filter(driver => {
+      return driver.toLowerCase() === name.toLowerCase();
+    });
+  };
+
+  const fuzzyMatch = (drivers, letters) => {
+    return drivers.filter(driver => {
+      return driver.toLowerCase().startsWith(letters.toLowerCase());
+    });
+  };
+
+  const matchName = (drivers, name) => {
+    return drivers.filter(driver => {
+      return driver.name.toLowerCase() === name.toLowerCase();
+    });
+  };
